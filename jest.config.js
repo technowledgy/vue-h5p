17a376ede314972eb696aeadd635c8f6d3e82fb2
Wolfgang Walther <wolfgangwalther@users.noreply.github.com>
2020-05-24 21:25:13 +0200

    chore: add coverage to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**'
+  ],
+  coverageReporters: [
+    'lcov',
+    'text-summary'
+  ],
   setupFiles: ['<rootDir>/tests/fetch.setup.js']
 }
